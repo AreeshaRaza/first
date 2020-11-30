25978071b76baf3af2903f48161e5e8772c63c38
AreeshaRaza <75206557+AreeshaRaza@users.noreply.github.com>
2020-11-29 22:05:41 -0800

    Add files via upload

---
+++ l 2b.py
@@ -0,0 +1,7 @@
+a=int(input('enter value of a'))
+b=int(input('enter value of b'))
+c=int(input('enter value of c'))
+X1=(-b+(b**2-4*a*c)**1/2)/2*a
+X2=(-b-(b**2-4*a*c)**1/2)/2*a
+print(X1)
+print(X2)
